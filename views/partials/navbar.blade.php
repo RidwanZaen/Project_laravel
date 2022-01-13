163a921b91da839928acd6feace590e8b775a08f
RidwanZaen <91907926+RidwanZaen@users.noreply.github.com>
2022-01-13 08:22:22 +0700

    Mengupload BackEnd
    mengupload view dan fungsi login dan register

---
+++ views/partials/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top " >
+      <div class="container">
+        <a class="navbar-brand" href="/">TICO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="/posts">Movie</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="/about">Feauture</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href='/register'>Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href='/login'>Log in</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
---
+++ web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\TodoController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::middleware(['check'])->group(function() {
+    Route::get('/hello', [HelloController::class, 'index']);   
+});
+
+Route::get('/login', [LoginController::class, 'index'])->name('login.index');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+Route::get('/logout', [LogoutController::class, 'index'])->name('logout');
