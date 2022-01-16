799b0cfeac20126af549feacda03b62138716d86
RidwanZaen <91907926+RidwanZaen@users.noreply.github.com>
2022-01-16 15:01:05 +0700

    update film.blade.php

--- views/film.blade.php
+++ views/film.blade.php
@@ -94,12 +94,15 @@ Rp 50.385</p>
     </div>
 
 </div>
-        <p>SINOPSIS</p>
+    <div class="container1" style="margin:10vh">
+    <p>SINOPSIS</p>
         <P>
             Pasca terbongkarnya identitas Spider-Man, Peter Parker meminta bantuan Doctor Strange. 
             Namun, ketika mantra salah, musuh berbahaya dari dunia lain mulai muncul, memaksa 
             Peter untuk menemukan apa artinya menjadi Spider-Man.
         </P>
+    </div>
+        
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
